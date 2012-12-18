b233c6d91e798fede6f23e433451ffdc61b2074a
Jon Turner <jturner@saveology.com>
2012-12-18 15:58:37 -0500

    adding rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'rspec'
+require 'save_campaigns'
+
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+  dev = File.join('config', 'development.yml')
+  YAML.load(File.open(dev)).each do |key, value|
+    ENV[key.to_s] = value
+  end if File.exists?(dev)
+end
